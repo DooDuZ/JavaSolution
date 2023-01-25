c43a381dd2b3565ab028fecd2577c4f161351532
DooDuZ <sin9158@naver.com>
2023-01-26 04:16:12 +0900

    동적계획법 6/7 완료

--- src/main/java/solution/levelTest/level17/test17_6.java
+++ src/main/java/solution/levelTest/level17/test17_6.java
@@ -6,36 +6,66 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class test17_6 {
-
+    static int[][] dp;
     static int N;
-    static int[][] price;
+    static int[][] cost;
+    // static boolean[][] visited;
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-
-        price = new int[N][3];
+        dp = new int[N][3];
+        cost = new int[N][3];
+        // visited = new boolean[N][3];
 
         for(int i = 0 ; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            price[i][0] = Integer.parseInt(st.nextToken());
-            price[i][1] = Integer.parseInt(st.nextToken());
-            price[i][2] = Integer.parseInt(st.nextToken());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
         }
 
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        sumCost(N-1, 0);
+        sumCost(N-1, 1);
+        sumCost(N-1, 2);
+
+        System.out.println(min);
     }
 
-    static void dfs(int depth, int value){
-        if(depth == N || value>=min){
-            min = Math.min(min, value);
+/*    static void dfs(int depth, int value, int color){
+        if(depth == N){
+            min = Math.min(value, min);
             return;
         }
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<3; j++){
-                dfs(depth + 1, value += price[i][j]);
+
+        for(int i = 0; i<3; i++){
+            if( !visited[depth][i] ){
+                visited[depth][i] = true;
+                dfs(depth+1, sumCost(depth, color), i);
+                visited[depth][i] = false;
             }
         }
+    }*/
+
+    static int sumCost( int depth, int color ){
+        if(dp[depth][color] == 0){
+            if(color==0){
+                dp[depth][color] = cost[depth][color] + Math.min( sumCost(depth-1, 1), sumCost(depth-1, 2) );
+            }else if(color==1){
+                dp[depth][color] = cost[depth][color] + Math.min( sumCost(depth-1, 0), sumCost(depth-1, 2) );
+            }else {
+                dp[depth][color] = cost[depth][color] + Math.min( sumCost(depth-1, 0), sumCost(depth-1, 1) );
+            }
+        }
+        if(depth==N-1 && dp[depth][color]<min){
+            min = dp[depth][color];
+        }
+        return dp[depth][color];
     }
 }
---
+++ src/main/java/solution/levelTest/level17/test17_7.java
@@ -0,0 +1,42 @@
+package solution.levelTest.level17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test17_7 {
+    static Integer[][] dp;
+    static int N;
+    static int[][] triangle;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new Integer[N][N];
+        triangle = new int[N][N];
+
+        StringTokenizer st;
+
+        for(int i = 0 ; i< N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j<=i; j++){
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+                if(i==N-1){
+                    dp[i][j] = triangle[i][j];
+                }
+            }
+        }
+
+        sumTriangle(0,0);
+        System.out.println(dp[0][0]);
+    }
+
+    public static int sumTriangle(int depth, int position){
+        if(dp[depth][position] == null){
+            dp[depth][position] = triangle[depth][position]
+                    + Math.max( sumTriangle(depth+1, position), sumTriangle(depth+1, position+1) );
+        }
+        return dp[depth][position];
+    }
+}
