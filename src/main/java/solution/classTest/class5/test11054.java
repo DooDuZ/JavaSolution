87a90cae98bda86eb6a8c9c3f1e92eef9530518a
sin91 <sin9158@naver.com>
2023-05-22 20:51:44 +0900

    BOJ class 4 가장 긴 바이토닉 부분 수열 완료

---
+++ src/main/java/solution/classTest/class5/test11054.java
@@ -0,0 +1,49 @@
+package solution.classTest.class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class test11054 {
+
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 증가하는 부분 길이 set
+        for(int i = 0;i < N; i++) {
+            dp[i] = 1;
+            for(int j = 0 ; j<i; j++) {
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        // 감소 부분 추가
+        for(int i = 0;i < N; i++) {
+            for(int j = 0 ; j<i; j++) {
+                if(arr[i] < arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+
+        System.out.println(dp[N-1]);
+    }
+}
