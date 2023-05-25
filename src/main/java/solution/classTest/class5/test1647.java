092516a489d8b3a9b153cd8ab3eba16be81114e5
sin91 <sin9158@naver.com>
2023-05-26 05:42:56 +0900

    BOJ class 5 도시 분리 계획 완료

--- src/main/java/solution/classTest/class5/test1647.java
+++ src/main/java/solution/classTest/class5/test1647.java
@@ -3,33 +3,37 @@ package solution.classTest.class5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class test1197 {
-    static int V;
-    static int E;
-    static ArrayList<ArrayList<Edge>> edgeList = new ArrayList<>();
+public class test1647 {
+    static int N;
+    static int M;
     static PriorityQueue<Edge> pq = new PriorityQueue<>();
+    static ArrayList<ArrayList<Edge>> edgeList = new ArrayList<>();
+
     static boolean[] visited;
+
     static int answer = 0;
+
     static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[V];
+        visited = new boolean[N];
 
-        for(int i = 0 ; i<V; i++){
+        for(int i = 0 ; i<N; i++){
             edgeList.add(new ArrayList<>());
         }
 
-        for(int i = 0 ; i<E; i++){
+        for(int i = 0 ; i<M; i++){
             st = new StringTokenizer(br.readLine());
-
             int from = Integer.parseInt(st.nextToken())-1;
             int to = Integer.parseInt(st.nextToken())-1;
             int cost = Integer.parseInt(st.nextToken());
@@ -38,54 +42,33 @@ public class test1197 {
             edgeList.get(to).add(new Edge(from, cost));
         }
 
+        // 임의의 노드에서 시작
         pq.add(new Edge(0,0));
 
         prim();
 
-        System.out.println(answer);
+        System.out.println(answer-max);
     }
 
-    static void prim(){
-        for(int i = 0 ; i<V; i++){
-            while(visited[pq.peek().v]){
+    public static void prim(){
+        for(int i = 0 ; i<N; i++){
+            while ( visited[pq.peek().v] ){
                 pq.poll();
             }
             Edge edge = pq.poll();
             visited[edge.v] = true;
             answer += edge.c;
 
+            max = Math.max(max, edge.c);
+
             ArrayList<Edge> edges = edgeList.get(edge.v);
 
-            for(int j = 0; j<edges.size(); j++){
-                if( visited[edges.get(j).v] ){
+            for(Edge e : edges){
+                if(visited[e.v]){
                     continue;
                 }
-                pq.add(edges.get(j));
+                pq.add(e);
             }
         }
     }
 }
-
-class Edge implements Comparable<Edge> {
-    int v;
-    int c;
-
-    Edge(int v, int c){
-        this.v = v;
-        this.c = c;
-    }
-
-    @Override
-    public int compareTo(Edge e){
-        return this.c - e.c;
-    }
-}
-
-/*
-5 5
-1 4 3
-2 3 3
-4 3 5
-2 1 6
-4 5 9
- */
