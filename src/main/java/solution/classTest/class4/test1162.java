e22e1a46453cc172bb2f336124bc728a7c0a8cc7
DooDuZ <sin9158@naver.com>
2023-10-13 23:02:12 +0900

    BOJ class 4 도로포장 완료

--- src/main/java/solution/classTest/class4/test1162.java
+++ src/main/java/solution/classTest/class4/test1162.java
@@ -6,10 +6,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class test1162 {
-    // 도로포장 진행중
-        // 메모리 초과가 난다,, ,어디서 어떻게...?
-
-    static int INF = 1000000000;
+    static Long INF = Long.MAX_VALUE;
     static int N;
     static int M;
     static int K;
@@ -25,21 +22,21 @@ public class test1162 {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        dist = new long[N][K+1];
+        dist = new long[N][K + 1];
 
-        for(int i = 0 ; i<N; i++){
+        for (int i = 0; i < N; i++) {
             Arrays.fill(dist[i], INF);
         }
 
-        for(int i = 0 ; i<N; i++){
+        for (int i = 0; i < N; i++) {
             edges.add(new ArrayList<>());
         }
 
-        for(int i = 0 ; i<M; i++){
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int from = Integer.parseInt(st.nextToken())-1;
-            int to = Integer.parseInt(st.nextToken())-1;
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
             int cost = Integer.parseInt(st.nextToken());
 
             edges.get(from).add(new CountEdge(to, cost, 0));
@@ -47,35 +44,30 @@ public class test1162 {
         }
 
         dist[0][0] = 0;
-
-        pq.add(new CountEdge(0, 0, 0));
+        pq.add(new CountEdge(0,0,0));
 
         dijkstra();
 
-        long answer = Long.MAX_VALUE;
+        Arrays.sort(dist[N-1]);
 
-        for(int i = 0; i<=K; i++){
-            answer = Math.min(dist[N-1][i], answer);
-        }
-
-        System.out.println(answer);
+        System.out.println(dist[N-1][0]);
     }
 
     public static void dijkstra(){
-        while (!pq.isEmpty()){
+        while(!pq.isEmpty()){
             CountEdge cur = pq.poll();
 
             if(dist[cur.v][cur.count] != cur.c){ continue; }
 
-            for( CountEdge e : edges.get(cur.v) ){
-                if(cur.count<K && cur.c < dist[e.v][cur.count+1] ){
-                    pq.add(new CountEdge(e.v, cur.c, cur.count+1));
+            for(CountEdge e : edges.get(cur.v)){
+                if(cur.count < K && cur.c < dist[e.v][cur.count+1]){
                     dist[e.v][cur.count+1] = cur.c;
+                    pq.add(new CountEdge(e.v, cur.c, cur.count+1));
                 }
 
-                if(e.c+cur.c < dist[e.v][cur.count]){
-                    pq.add(new CountEdge(e.v, e.c+cur.c, cur.count));
+                if(cur.c+e.c < dist[e.v][cur.count]){
                     dist[e.v][cur.count] = cur.c+e.c;
+                    pq.add(new CountEdge(e.v, cur.c+e.c, cur.count));
                 }
             }
         }
@@ -95,9 +87,9 @@ class CountEdge implements Comparable<CountEdge> {
     @Override
     public int compareTo(CountEdge e){
         if(e.c>this.c){
-            return 1;
-        }else if(e.c<this.c){
             return -1;
+        }else if(e.c<this.c){
+            return 1;
         }
         return 0;
     }
