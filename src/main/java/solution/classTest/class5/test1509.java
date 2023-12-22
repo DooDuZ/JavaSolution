8763bfffaf8a280943fc85b17d88f31c28d5d277
DooDuZ <sin9158@naver.com>
2023-12-23 04:57:06 +0900

    BOJ class 5 펠린드롬 분할 완료

--- src/main/java/solution/classTest/class5/test1509.java
+++ src/main/java/solution/classTest/class5/test1509.java
@@ -3,12 +3,14 @@ package solution.classTest.class5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class test1509 {
 
-    static Integer[][] dp;
-
+    static Boolean[][] palindrome;
     static char[] sequence;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -16,25 +18,50 @@ public class test1509 {
 
         sequence = str.toCharArray();
 
-        dp = new Integer[str.length()][str.length()];
+        palindrome = new Boolean[str.length() + 1][str.length() + 1];
 
-        for (int i = 0; i < str.length(); i++) {
-            dp[i][i] = 1;
+        dp = new int[str.length() + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= str.length(); i++) {
+            palindrome[i][i] = true;
         }
 
+        for (int i = 1; i < str.length(); i++) {
+            if (sequence[i - 1] == sequence[i]) {
+                palindrome[i][i + 1] = true;
+            } else {
+                palindrome[i][i + 1] = false;
+            }
+        }
 
+        setDp(str.length());
+
+        System.out.println(dp[str.length()]);
     }
 
-    static int check(int start, int end) {
-        if (dp[start][end] == null) {
-            if(sequence[start] == sequence[end]){
-                if(end-start == 1 || check(start+1, end-1)==1){
-                    return 1;
+    public static int setDp(int end) {
+        if (dp[end] == Integer.MAX_VALUE) {
+            for (int i = 1; i <= end; i++) {
+                if (palin(i, end)) {
+                    dp[end] = Math.min(dp[end], setDp(i - 1) + 1);
                 }
             }
-            return 0;
         }
 
-        return dp[start][end];
+        return dp[end];
+    }
+
+    public static boolean palin(int start, int end) {
+        if (palindrome[start][end] == null) {
+            if (sequence[start - 1] == sequence[end - 1]) {
+                palindrome[start][end] = palin(start + 1, end - 1);
+            } else {
+                palindrome[start][end] = false;
+            }
+        }
+
+        return palindrome[start][end];
     }
 }
