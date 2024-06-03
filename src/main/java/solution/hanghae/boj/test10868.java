1be857f29a3968b3145f2ee07374e2d62cad85f6
DooDuZ <sin9158@naver.com>
2024-06-04 03:01:26 +0900

    BOJ 최솟값 완료 / 주석 추가

--- src/main/java/solution/hanghae/boj/test10868.java
+++ src/main/java/solution/hanghae/boj/test10868.java
@@ -40,6 +40,7 @@ public class test10868 {
         int start, end, min;
         Node parent, left, right;
 
+        // 루트 노드용 생성자
         Node(int start, int end) {
             this.start = start;
             this.end = end;
@@ -47,14 +48,18 @@ public class test10868 {
             init();
         }
 
+        // 배열 범위를 생성자 파라미터로 입력 받습니다.
         Node(int start, int end, Node parent) {
             this.start = start;
             this.end = end;
             this.parent = parent;
 
+            // 세팅 시작
             init();
         }
 
+        // 범위를 기준으로 좌/우로 나눠 노드를 생성해 left, right에 넣어줍니다.
+        // 범위가 최소 단위라면, 배열 값을 min으로 지정해줍니다.
         private void init() {
             if (start != end) {
                 int mid = (start + end) / 2;
@@ -69,17 +74,21 @@ public class test10868 {
             }
         }
 
+        // 범위를 기준으로 탐색합니다.
         public int search(int s, int e) {
+            // 범위가 완전히 일치하면 최소값 return
             if (s == this.start && e == this.end) {
                 return this.min;
             }
 
             int ret = 1_000_000_001;
 
+            // 시작점이 왼쪽 노드의 끝점보다 작거나 같다면 왼쪽 노드의 끝점/현재 탐색지점의 끝 점중 작은 값을 범위로 넘겨줍니다.
             if (s <= left.end) {
                 ret = Math.min(left.search(s, Math.min(left.end, e) ), ret);
             }
 
+            // 끝 점이 오른쪽 노드의 시작점보다 크거나 같다면 현재 시작점과 오른쪽 노드의 시작점 중 큰 값을 시작 지점으로 넘겨줍니다.
             if (e >= right.start) {
                 ret = Math.min(right.search(Math.max(right.start, s), e), ret);
             }
@@ -88,18 +97,3 @@ public class test10868 {
         }
     }
 }
-
-/*
-10 1
-75
-30
-100
-38
-50
-51
-52
-20
-81
-5
-3 5
- */
