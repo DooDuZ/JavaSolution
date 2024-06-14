b4c975889c6e6b4dfa8f5f0ed76311be9ee5b76b
DooDuZ <sin9158@naver.com>
2024-06-14 16:04:23 +0900

    counting sort 추가

---
+++ src/main/java/solution/implement_sort/CountingSort.java
@@ -0,0 +1,75 @@
+package solution.implement_sort;
+
+public class CountingSort {
+    int min;
+    int max;
+
+    // 외부에서 호출되는 메서드
+    public int[] sort(int[] arr) {
+        return countingSort(arr, getCountArray(arr));
+    }
+
+    private int[] countingSort(int[] arr, int[] count) {
+        // 입력 배열에 있는 각 값의 개수를 센다
+        count(arr, count);
+        // counting 배열 누적합
+        prefixSum(count);
+
+        int[] sorted = new int[arr.length];
+
+        // 안정 상태 유지를 위해 뒤부터 순회
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            // 0인덱스이므로 - 1
+            sorted[count[arr[i] - min] - 1] = arr[i];
+            count[arr[i] - min]--;
+        }
+
+        return sorted;
+    }
+
+    private void count(int[] arr, int[] count) {
+        for (int number : arr) {
+            // arr의 value를 count 배열의 index로 사용한다
+            count[number - min] += 1;
+        }
+    }
+
+    private void prefixSum(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            numbers[i] = numbers[i] + numbers[i - 1];
+        }
+    }
+
+    private int[] getCountArray(int[] arr) throws IllegalArgumentException {
+        findRangeBound(arr);
+        // 유효 범위가 아니라면 exception이 발생한다.
+        validateRange();
+
+        return new int[max - min + 1];
+    }
+
+    private void validateRange() {
+        // 찾은 범위로 배열을 생성한다.
+        // 범위가 천만을 넘어가면 exception을 발생 시킨다.
+        if (max - min >= 10_000_000) {
+            throw new IllegalArgumentException("범위 초과");
+        }
+    }
+
+    private void findRangeBound(int[] arr) {
+        // count 배열 생성을 위한 범위가 필요하다.
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        // N번 순회하며 최대값과 최소값을 찾는다
+        for (int number : arr) {
+            max = Math.max(max, number);
+            min = Math.min(min, number);
+        }
+
+        // 클래스의 필드 멤버에 저장
+        this.max = max;
+        this.min = min;
+    }
+}
--- src/main/java/solution/implement_sort/MergeSort.java
+++ src/main/java/solution/implement_sort/MergeSort.java
@@ -1,38 +1,56 @@
 package solution.implement_sort;
 
+
 public class MergeSort {
+
+    // 외부에서 호출되는 메서드
     public void sort(int[] arr) {
         mergeSort(arr);
     }
 
-
+    // 최초 호출
+    // 배열을 입력 받고, 정렬에 임시로 사용할 sorted배열을 선언해서 넘겨준다
     private void mergeSort(int[] arr) {
         int[] sorted = new int[arr.length];
 
         mergeSort(0, arr.length - 1, sorted, arr);
     }
 
-
+    // 인자로 좌/우 범위의 인덱스를 받는다
+    // 매 비교마다 배열을 생성한다면 메모리 할당을 계속해서 해야한다
+    // 파라미터로 받아온 sorted배열에 각 인덱스를 통해 값을 저장한다
     private void mergeSort(int left, int right, int[] sorted, int[] arr) {
+        // 좌, 우 값이 같다면 값이 하나만 있으므로 return
         if (left == right) {
             return;
         }
 
+        // 중간 값을 구하고
         int mid = (left + right) / 2;
 
+        // 양쪽으로 나눠서 재귀 호출
         mergeSort(left, mid, sorted, arr);
         mergeSort(mid + 1, right, sorted, arr);
 
+        // 양쪽 배열 병합
         merge(left, right, sorted, arr);
     }
 
+    // 역시 좌/우 값을 파라미터로 받는다
     private void merge(int left, int right, int[] sorted, int[] arr) {
+
+        // 저장용 배열의 인덱스. 가장 왼쪽 값부터 시작
         int index = left;
+        // 왼쪽은 시작점부터
         int l = left;
         int mid = (left + right) / 2;
+        // 오른쪽 배열은 중간값 + 1부터 시작
         int r = mid + 1;
 
+        // 양쪽 배열 중 한쪽의 모든 값을 다 사용할 때까지
         while (l <= mid && r <= right) {
+            // 왼쪽이 오른쪽 보다 작거나 같다면 왼쪽 값을 임시 배열에 삽입한다.
+            // 만약 <=이 아닌 <를 사용한다면 뒷 순서의 값이 앞쪽으로 삽입되므로 stable한 상태가 깨지게 된다.
             if (arr[l] <= arr[r]) {
                 sorted[index] = arr[l];
                 l++;
@@ -43,6 +61,7 @@ public class MergeSort {
             index++;
         }
 
+        // 한쪽 배열 다 채운 뒤, 남은 값을 끝까지 채워준다
         if (l <= mid) {
             while (index <= right) {
                 sorted[index] = arr[l];
@@ -55,6 +74,8 @@ public class MergeSort {
             }
         }
 
+        // 주어진 구간의 값을 정렬해서 교체하는 과정
+        // 임시 배열의 값을 원본 배열로 복사해준다
         for (int i = left; i <= right; i++) {
             arr[i] = sorted[i];
         }
